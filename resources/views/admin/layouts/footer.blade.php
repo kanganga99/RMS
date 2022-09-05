7f982b5d1ceda43b15ecc8e692f1e8a96f6f33ef
299hannah <hannahmwangiwambui@gmail.com>
2022-09-05 13:48:46 +0300

    admin side ui with skeleton project

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <strong>Copyright &copy;2022 <a href="">Admin</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
+    </div>
+@section('headsection')
+@show
+  </footer>
+
+  
