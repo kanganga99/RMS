6ab97961c2373747fc3229bd5c525e6525ca15c6
kanganga99 <kangangamargaret2020@gmail.com>
2022-09-24 14:01:51 +0300

    restructure

--- resources/views/agent/agent_register.blade.php
+++ resources/views/agent/agent_register.blade.php
@@ -28,6 +28,8 @@
           <ul>
               @foreach ($errors->all() as $error)
                   <li>{{ $error }}</li>
+
+                  
               @endforeach
           </ul>
       </div>
